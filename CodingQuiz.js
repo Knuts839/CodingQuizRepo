63af522967a403c212d887bdab64088c5d00dfb4
Ravi <knuts839@umn.edu>
2020-09-28 17:10:20 -0500

    I built inevent listeners applied the same css to the 4 question ids I've created so far and it displays well in browser

--- CodingQuiz.js
+++ CodingQuiz.js
@@ -1,3 +1,4 @@
+// Start button is displaying default
 var startButton = document.querySelector("#StartQuiz")
 
 startButton.addEventListener("click", StartQuiz)
@@ -7,3 +8,21 @@ function StartQuiz() {
     document.getElementById("CodingQuizStart").style.display = "none";
     document.getElementById("Q1").style.display = "initial";
 }
+// Q1 displays to none and Q2 sets display to initial
+var Q1B1 = document.querySelector("#Q1B1");
+var Q1B2 = document.querySelector("#Q1B2");
+var Q1B3 = document.querySelector("#Q1B3");
+var Q1B4 = document.querySelector("#Q1B4");
+
+
+Q1B1.addEventListener("click", Q1)
+Q1B2.addEventListener("click", Q1)
+Q1B3.addEventListener("click", Q1)
+Q1B4.addEventListener("click", Q1)
+
+
+function Q1() {
+    var Que1 = document.querySelector("Q1");
+    document.getElementById("Q1").style.display = "none";
+    document.getElementById("Q2").style.display = "initial";
+} // Q2 displays to none and Q3 sets display to initial
