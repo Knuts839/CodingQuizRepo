bfdddde01ba0fd3b2a0f50ad5bdc62007737c419
Ravi <knuts839@umn.edu>
2020-09-29 00:07:18 -0500

    added timer functionality

--- CodingQuiz.js
+++ CodingQuiz.js
@@ -4,9 +4,41 @@ var startButton = document.querySelector("#StartQuiz")
 startButton.addEventListener("click", StartQuiz)
 
 function StartQuiz() {
-    var CodingQuizStart = document.querySelector("CodingQuizStart");
+    Countdown();
     document.getElementById("CodingQuizStart").style.display = "none";
     document.getElementById("Q1").style.display = "initial";
+}
+
+function Countdown() {
+    // Set the date we're counting down to
+    var countDownDate = 121; // new Date().getTime() + 75000 + 60000; // Rube Golberg
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        countDownDate -= 1;
+        var seconds = countDownDate; //Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (countDownDate < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+
 }
 
 /////////////////////////////////////////////////////////////
